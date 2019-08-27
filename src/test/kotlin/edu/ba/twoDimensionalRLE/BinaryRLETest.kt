8cebf4291531726c24e2cf4a12b843e9da559045
Sven Fiergolla <sven.fiergolla@gmail.com>
2019-08-27 16:33:32 +0200

    cleanup and applying stashes

--- src/test/kotlin/edu/ba/twoDimensionalRLE/BinaryRLETest.kt
+++ src/test/kotlin/edu/ba/twoDimensionalRLE/BinaryRLETest.kt
@@ -1,18 +1,24 @@
 package edu.ba.twoDimensionalRLE
 
 import edu.ba.twoDimensionalRLE.encoder.BinaryRunLengthEncoder
-import org.junit.Test
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
 
-class BinaryRLETest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+    class BinaryRLETest {
 
     private val binaryRunLengthEncoder = BinaryRunLengthEncoder()
 
     @Test
+    @Order(1)
     fun encodeFile() {
         binaryRunLengthEncoder.encode("data/t8.shakespeare.txt")
     }
 
     @Test
+    @Order(2)
     fun decodeFile() {
         binaryRunLengthEncoder.decode("data/encoded/t8.shakespeare.txt_bin_rle_nr")
     }
