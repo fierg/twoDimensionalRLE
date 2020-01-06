512832764289fb7b1fc2b0a034056e86d0e9991c
Sven Fiergolla <sven.fiergolla@gmail.com>
2020-01-06 17:48:04 +0100

    Improved BWT, Resolves #44

--- src/main/kotlin/edu/ba/twoDimensionalRLE/tranformation/BurrowsWheelerTransformationLinearTime.kt
+++ src/main/kotlin/edu/ba/twoDimensionalRLE/tranformation/BurrowsWheelerTransformationLinearTime.kt
@@ -1,5 +1,9 @@
 package edu.ba.twoDimensionalRLE.tranformation
 
+import edu.ba.twoDimensionalRLE.extensions.shift
+import java.lang.IllegalArgumentException
+
+@ExperimentalStdlibApi
 class BurrowsWheelerTransformationLinearTime {
 
 
@@ -10,8 +14,82 @@ class BurrowsWheelerTransformationLinearTime {
         return Pair(String(table.map { it[it.lastIndex] }.toCharArray()), index)
     }
 
-    fun inverseTransform(input: String, index: Int) {
+
+    fun transformByteArray(input: ByteArray): Pair<ByteArray, Int> {
+        val table = Array(input.size) { input.shift(it)}
+        table.sort()
+        val index = table.indexOfFirst { it.contentEquals(input) }
+
+        return Pair(table.map { it[it.lastIndex] }.toByteArray(), index)
+    }
 
 
+    fun inverseTransform(L: String, index: Int): String {
+
+        // corresponding to D1. [find first characters of rotations]
+        val F = L.toCharArray().sortedArray()
+
+        // corresponding to D2. [build list of predecessor characters]
+        val P = IntArray(L.length)
+        val C = mutableMapOf<Byte, Int>()
+
+        for (i in 0 until L.length) {
+            P[i] = C.getOrDefault(L[i].toByte(), 0)
+            C[L[i].toByte()] = C.getOrDefault(L[i].toByte(), 0) + 1
+        }
+
+        var sum = 0
+
+        for (i in 0 until 256) {
+            if (C.containsKey(i.toByte())) {
+                sum += C[i.toByte()]!!
+                C[i.toByte()] = sum - C[i.toByte()]!!
+            }
+        }
+
+        val S = CharArray(L.length)
+
+        // D3. [form output S]
+        var i = index
+        for (j in L.length -1 downTo 0){
+            S[j] = L[i]
+            i = P[i] + C.getOrElse(L[i].toByte()) {throw IllegalArgumentException("Unexpected index!")}
+        }
+
+        return S.concatToString()
+    }
+
+    fun inverseTransformByteArray(L: String, index: Int): String {
+
+        // corresponding to D1. [find first characters of rotations]
+        val F = L.toCharArray().sortedArray()
+
+        // corresponding to D2. [build list of predecessor characters]
+        val P = IntArray(L.length)
+        val C = mutableMapOf<Byte, Int>()
+
+        for (i in 0 until L.length) {
+            P[i] = C.getOrDefault(L[i].toByte(), 0)
+            C[L[i].toByte()] = C.getOrDefault(L[i].toByte(), 0) + 1
+        }
+
+        var sum = 0
+
+        for (i in 0 until 256) {
+            if (C.containsKey(i.toByte())) {
+                sum += C[i.toByte()]!!
+                C[i.toByte()] = sum - C[i.toByte()]!!
+            }
+        }
+
+        val S = CharArray(L.length)
+
+        var i = index
+        for (j in L.length -1 downTo 0){
+            S[j] = L[i]
+            i = P[i] + C.getOrElse(L[i].toByte()) {throw IllegalArgumentException("Unexpected index!")}
+        }
+
+        return S.concatToString()
     }
 }
--- src/test/kotlin/edu/ba/twoDimensionalRLE/transformation/LinearBurowsWheelerTransformationTest.kt
+++ src/test/kotlin/edu/ba/twoDimensionalRLE/transformation/LinearBurowsWheelerTransformationTest.kt
@@ -8,9 +8,12 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 
 
+@ExperimentalStdlibApi
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class LinearBurowsWheelerTransformationTest {
     private var log = Log.kotlinInstance()
+    private val word ="abraca"
+    private val longWord = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
     init {
         log.newFormat {
@@ -23,8 +26,35 @@ class LinearBurowsWheelerTransformationTest {
     @Order(1)
     fun simpleTest() {
         val bwtl = BurrowsWheelerTransformationLinearTime()
-        val result = bwtl.transform("bcbccbcbcabbaaba")
+        log.info("Strarting transformation on $word ...")
+        val result = bwtl.transform(word)
 
         log.info("Result: ${result.first}, Index: ${result.second}")
     }
+
+    @Test
+    @Order(2)
+    fun simpleTestDecode() {
+        val bwtl = BurrowsWheelerTransformationLinearTime()
+        val result = bwtl.transform(word)
+        log.info("Strarting inverse transformation on ${result.first} ...")
+
+        val inverted = bwtl.inverseTransform(result.first, result.second)
+
+        log.info("Result: $inverted")
+    }
+
+    @Test
+    @Order(3)
+    fun longTestDecode() {
+        val bwtl = BurrowsWheelerTransformationLinearTime()
+        val result = bwtl.transform(longWord)
+        log.info("Strarting inverse transformation on ${result.first} ...")
+
+        val inverted = bwtl.inverseTransform(result.first, result.second)
+
+        log.info("Result: $inverted")
+    }
+
+
 }
