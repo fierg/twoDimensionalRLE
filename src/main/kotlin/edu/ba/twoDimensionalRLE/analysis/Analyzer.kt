25c9b0365cc5cca114488b0d763f4d6169e5919c
Sven Fiergolla <sven.fiergolla@gmail.com>
2020-01-24 21:55:07 +0100

    Added license

--- src/main/kotlin/edu/ba/twoDimensionalRLE/analysis/Analyzer.kt
+++ src/main/kotlin/edu/ba/twoDimensionalRLE/analysis/Analyzer.kt
@@ -1,5 +1,29 @@
 package edu.ba.twoDimensionalRLE.analysis
 
+/*
+MIT License
+
+Copyright (c) 2019 Sven Fiergolla
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
 import de.jupf.staticlog.Log
 import de.jupf.staticlog.core.LogLevel
 import edu.ba.twoDimensionalRLE.extensions.round
@@ -7,8 +31,6 @@ import loggersoft.kotlin.streams.BitStream
 import loggersoft.kotlin.streams.openBinaryStream
 import java.io.File
 import java.nio.file.Files
-import kotlin.math.roundToInt
-import kotlin.math.roundToLong
 
 @ExperimentalUnsignedTypes
 class Analyzer {
--- src/main/kotlin/edu/ba/twoDimensionalRLE/encoder/huffman/HuffmanEncoder.kt
+++ src/main/kotlin/edu/ba/twoDimensionalRLE/encoder/huffman/HuffmanEncoder.kt
@@ -1,5 +1,28 @@
 package edu.ba.twoDimensionalRLE.encoder.huffman
 
+/*
+MIT License
+
+Copyright (c) 2019 Sven Fiergolla
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 
 import de.jupf.staticlog.Log
 import de.jupf.staticlog.core.LogLevel
@@ -14,7 +37,7 @@ import java.util.*
 import kotlin.experimental.or
 
 @ExperimentalUnsignedTypes
-class HuffmanEncoder(var DEBUGLOG: Boolean = false) : Encoder, RangedEncoder {
+class HuffmanEncoder(DEBUGLOG: Boolean = false) : Encoder, RangedEncoder {
     private val log = Log.kotlinInstance()
     val mapping = mutableMapOf<Byte, StringBuffer>()
     private val byteSize = 8
