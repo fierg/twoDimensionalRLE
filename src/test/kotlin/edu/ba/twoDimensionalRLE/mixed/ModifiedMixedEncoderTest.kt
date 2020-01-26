2d11cfd55b1a3ce1c3939e6e460b39910fad8ca9
Sven Fiergolla <sven.fiergolla@gmail.com>
2020-01-26 14:52:28 +0100

    WIP

--- src/test/kotlin/edu/ba/twoDimensionalRLE/mixed/ModifiedMixedEncoderTest.kt
+++ src/test/kotlin/edu/ba/twoDimensionalRLE/mixed/ModifiedMixedEncoderTest.kt
@@ -19,7 +19,7 @@ class ModifiedMixedEncoderTest {
     private var log = Log.kotlinInstance()
     val applyByteMapping = true
     val applyBurrowsWheelerTransformation = true
-    val applyHuffmanEncoding = true
+    val applyHuffmanEncoding = false
 
     init {
         log.newFormat {
@@ -35,7 +35,7 @@ class ModifiedMixedEncoderTest {
 
     private val mixedEncoder = ModifiedMixedEncoder()
 
-    @Test
+    //@Test
     @Order(5)
     fun encodeVertReadingRLEVaryingNrsA() {
         log.info("Encoding with vertical byte reading and binary RLE.")
@@ -100,6 +100,112 @@ class ModifiedMixedEncoderTest {
 
 
 
+
+        log.info("all combinations tried. result:")
+        println(
+            resultMap.toList().sortedBy
+            { (_, value) -> value }.toMap()
+        )
+    }
+
+    @Test
+    @Order(5)
+    fun encodeVertReadingRLEVaryingNrs1() {
+        log.info("Encoding with vertical byte reading and binary RLE.")
+
+        val resultMap = mutableMapOf<Map<Int, Int>, Long>()
+
+        val mapset = mutableSetOf<Map<Int, Int>>()
+
+        mapset.add(mapOf(0 to 2, 1 to 2, 2 to 2, 3 to 2, 4 to 3, 5 to 4, 6 to 3, 7 to 6))
+        mapset.add(mapOf(0 to 2, 1 to 2, 2 to 2, 3 to 2, 4 to 3, 5 to 4, 6 to 3, 7 to 7))
+
+        mapset.add(mapOf(0 to 2, 1 to 2, 2 to 3, 3 to 3, 4 to 3, 5 to 4, 6 to 5, 7 to 8))
+        mapset.add(mapOf(0 to 2, 1 to 2, 2 to 2, 3 to 3, 4 to 3, 5 to 5, 6 to 7, 7 to 9))
+
+        mapset.add(mapOf(0 to 4, 1 to 4, 2 to 4, 3 to 4, 4 to 4, 5 to 6, 6 to 6, 7 to 8))
+        mapset.add(mapOf(0 to 4, 1 to 4, 2 to 4, 3 to 4, 4 to 4, 5 to 6, 6 to 7, 7 to 9))
+
+        mapset.add(mapOf(0 to 4, 1 to 4, 2 to 4, 3 to 4, 4 to 5, 5 to 7, 6 to 8, 7 to 8))
+        mapset.add(mapOf(0 to 4, 1 to 4, 2 to 4, 3 to 4, 4 to 5, 5 to 7, 6 to 8, 7 to 10))
+
+
+
+        if (applyByteMapping) log.info("Encoding with mapping as preprocessing.")
+        if (applyBurrowsWheelerTransformation) log.info("Encoding with a Burrows Wheeler Transformation as preprocessing.")
+        val deferred = mutableListOf<Deferred<Any>>()
+        val context = newFixedThreadPoolContext(10, "co")
+
+        for (i in 2..4) {
+            for (j in 2..4) {
+                for (k in 2..4) {
+                    for (l in 2..4) {
+                        for (m in 2..5) {
+                            for (n in 2..7) {
+                                for (o in 2..8) {
+                                    for (p in 2..10) {
+
+                                        deferred.add(CoroutineScope(context).async {
+
+                                            val bitsPerNumberMapping =
+                                                mapOf(0 to i, 1 to j, 2 to k, 3 to l, 4 to m, 5 to n, 6 to o, 7 to p)
+
+                                            if (mapset.contains(bitsPerNumberMapping)) {
+
+                                                if (File("${encodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p").exists()) {
+                                                    log.info("deleting directory: ${encodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p")
+                                                    File("${encodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p").deleteRecursively()
+                                                    File("${decodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p").deleteRecursively()
+
+
+                                                }
+                                                log.info("creating directory: ${encodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p")
+                                                File("${encodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p").mkdirs()
+                                                File("${decodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p").mkdirs()
+
+                                                File(folderToEncode).listFiles().forEach {
+                                                    try {
+                                                        mixedEncoder.encodeInternal(
+                                                            "${folderToEncode}/${it.name}",
+                                                            "${encodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p/${it.name}.mixed",
+                                                            applyByteMapping,
+                                                            applyBurrowsWheelerTransformation,
+                                                            applyHuffmanEncoding,
+                                                            bitsPerNumberMapping
+
+                                                        )
+                                                    } catch (e: Exception) {
+                                                        log.error(e.toString(), e)
+                                                    }
+                                                }
+                                                log.info("Finished encoding of corpus.")
+
+                                                val result = Analyzer().sizeCompare(
+                                                    folderToEncode,
+                                                    "${encodeFolder}/CalgaryCorpus/$i$j$k$l$m$n$o$p",
+                                                    "mixed",
+                                                    null,
+                                                    bitsPerNumberMapping,
+                                                    true
+                                                )
+                                                resultMap[bitsPerNumberMapping] = result
+                                                return@async
+                                            } else {
+                                                return@async
+                                            }
+                                        })
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+        runBlocking {
+            return@runBlocking deferred.map { it.await() }
+        }
 
         log.info("all combinations tried. result:")
         println(
